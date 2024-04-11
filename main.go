c772e2177c7d6b8cc0ebdb473712864d2a034b8c
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2024-04-11 08:45:06 -0400

    Bot responds with weather (formatted version)

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
-  "log"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
@@ -18,7 +18,7 @@ var (
 )
 
 func main() {
-  log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	err := godotenv.Load()
 
 	var (
@@ -55,24 +55,23 @@ func main() {
 	commandHandlers := map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
 		"weather": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			var city string
-      var response string
+			var response string
 
 			// Loop through the options to find the "city" option
 			for _, option := range i.ApplicationCommandData().Options {
 				if option.Name == "city" {
-					city = option.Value.(string)	
+					city = option.Value.(string)
 
-          weatherService := services.NewScheduleService(city)
-          weatherData, err := weatherService.FetchWeatherData()
+					weatherService := services.NewScheduleService(city)
+					weatherData, err := weatherService.FetchWeatherData()
 
-          if err != nil {
-            log.Println("An Error occurred")
-          }
+					if err != nil {
+						log.Println("An Error occurred")
+					}
 
-          response = fmt.Sprintf("Weather for %s\nTeampurature is %.2f°C\nTemp Low %.2f°C\nTemp High %.2f°C\n",
-            city, weatherData.Main.Temp, weatherData.Main.TempMin, weatherData.Main.TempMax)
+					response = fmt.Sprintf("Weather for %s\nTeampurature is %.2f°C\nTemp Low %.2f°C\nTemp High %.2f°C\n",
+						city, weatherData.Main.Temp, weatherData.Main.TempMin, weatherData.Main.TempMax)
 
-                    
 					break
 				}
 			}
--- services/weather.go
+++ services/weather.go
@@ -10,46 +10,45 @@ import (
 	"github.com/luka2220/discGo/models"
 )
 
-
 type WeatherDataService struct {
-  URL string  
+	URL string
 }
 
 func NewScheduleService(city string) *WeatherDataService {
-  url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=4b131da2c3d34ffbf959576862e58b66&units=metric", city)
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=4b131da2c3d34ffbf959576862e58b66&units=metric", city)
 
-  return &WeatherDataService {
-    url, 
-  }
+	return &WeatherDataService{
+		url,
+	}
 }
 
 func (w *WeatherDataService) FetchWeatherData() (*models.Weather, error) {
-  log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-  response, err := http.Get(w.URL)
-  if err != nil {
-    log.Panic("An error occured fetching weather data", err)
-    return nil, err
-  }
-  
-  // Close the body once the function execution returns
-  defer response.Body.Close()
+	response, err := http.Get(w.URL)
+	if err != nil {
+		log.Panic("An error occured fetching weather data", err)
+		return nil, err
+	}
 
-  log.Printf("Response status: %s\n", response.Status)
+	// Close the body once the function execution returns
+	defer response.Body.Close()
 
-  scanner := bufio.NewScanner(response.Body)
+	log.Printf("Response status: %s\n", response.Status)
 
-  var weatherDataBytes []byte
-  var weatherData *models.Weather
+	scanner := bufio.NewScanner(response.Body)
 
-  for i := 0; scanner.Scan() && i < 5; i++ {
+	var weatherDataBytes []byte
+	var weatherData *models.Weather
+
+	for i := 0; scanner.Scan() && i < 5; i++ {
 		weatherDataBytes = scanner.Bytes()
 	}
 
-  if err := json.Unmarshal(weatherDataBytes, &weatherData); err != nil {
+	if err := json.Unmarshal(weatherDataBytes, &weatherData); err != nil {
 		log.Panic("Error occured when unmarshalling data", err)
-    return nil, err
+		return nil, err
 	}
 
- return weatherData, nil 
+	return weatherData, nil
 }
